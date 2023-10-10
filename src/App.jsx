188be1009b2dca37607edb9ac6365141d3d29853
Harriss <harissitthikorn@gmail.com>
2023-10-10 10:41:00 +0700

    10/10/66

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
 
 function App() {
 
   return (
-    <h1>
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>Welcome To Homecomming</div>
+    )
+}
+
+export default Home
